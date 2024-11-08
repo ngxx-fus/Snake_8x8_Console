5e80c3a36317db66001621f89c3087f4d8bc7c26
ngxx-fus <msnp@outlook.com.vn>
2024-11-08 10:36:37 +0700

    resize code

--- With_C/SnakeC.h
+++ With_C/SnakeC.h
@@ -1,4 +1,3 @@
-#include <time.h>
 #include <stdlib.h>
 
 #include "Base_Lib.h"
@@ -30,57 +29,53 @@
 
 //Map value
 enum enum_Apple{apple=19};
-enum enum_Snake{head=3, body_r=15, body_u=105, body_l=195, body_d=285, tail=7};
+enum enum_Snake{head=3, body_r=17, body_u=32, body_l=47, body_d=62, tail=7};
 // Next Direction earlier phase Current Direction 15 degrees
-enum enum_NextDirection{to_ward=0, to_right=15, to_up=105, to_left=195, to_down=285};
+enum enum_NextDirection{to_ward=0, to_right=15, to_up=30, to_left=45, to_down=60};
 // Current Direction 
-enum enum_CurrDirection{right=0, up=90, left=180, down=270};
+enum enum_CurrDirection{right=0, up=15, left=30, down=45};
 
-#define _360_bounded(x) (((x)+360)%360)
 #define _mod8_bounded(x) ((((x)+8)%8))
-#define _point_eq(Ax,Ay,Bx,By) ((((Ax)==(Bx))&&((Ay)==(By)))?(1):(0))
+#define _polong_eq(Ax,Ay,Bx,By) ((((Ax)==(Bx))&&((Ay)==(By)))?(1):(0))
 
 //This will avail for 1-8 and 0->7 :>
-int field[8][8];
-//Game is paused?
-int is_paused = 0;
+uint8 field[8][8];
 //Game over?
-int is_dead = 0;
+uint8 is_dead = 0;
 //next direction from current
 //to-ward: +0, turn-left: +90, turn-right: +270, u-turn: +180
-int next_direction = 0; 
+uint8 next_direction = 0; 
 //current direction
 //UP: 90, LEFT: 180, RIGHT: 0, DOWN: 270
-int current_direction = 0;
+uint8 current_direction = 0;
 //snake's head
-int head_x = 0;
-int head_y = 0;
+uint8 head_x = 0;
+uint8 head_y = 0;
 //apple
-int apple_x = 0;
-int apple_y = 0;
+uint8 apple_x = 0;
+uint8 apple_y = 0;
+//for random
+static uint8 amwixpt;
 
-void random_XY(int* X, int* Y){
+void random_XY(uint8* X, uint8* Y){
 
-    srand(time(0));
+    srand(amwixpt++);
     do{
-        *X = _mod8_bounded(random());
-        *Y = _mod8_bounded(random());
+        *X = _mod8_bounded(rand());
+        *Y = _mod8_bounded(rand());
     } while(field[*X][*Y] != 0);
 }
 
 void random_apple(){
-    int X, Y;
     random_XY(&apple_x, &apple_y);
     field[apple_x][apple_y] = apple;
 }
 
-void direction_change(int new_direction){
+void direction_change(uint8 new_direction){
     next_direction = new_direction;
 }
 
 void direction_update(){
-    // current_direction += next_direction;
-    // current_direction = _360_bounded(current_direction);
     switch (next_direction) {
         case to_ward:
             return;
@@ -89,33 +84,33 @@ void direction_update(){
                 next_direction = to_ward;
                 return;
             }
-            current_direction = _360_bounded(next_direction-15);
+            current_direction = next_direction-15;
             return;
         case to_right:
             if(current_direction == left) {
                 next_direction = to_ward;
                 return;
             }
-            current_direction = _360_bounded(next_direction-15);
+            current_direction = next_direction-15;
             return;
         case to_up:
             if(current_direction == down) {
                 next_direction = to_ward;
                 return;
             }
-            current_direction = _360_bounded(next_direction-15);
+            current_direction = next_direction-15;
             return;
         case to_down:
             if(current_direction == up) {
                 next_direction = to_ward;
                 return;
             }
-            current_direction = _360_bounded(next_direction-15);
+            current_direction = next_direction-15;
             return;
     }
 }
 
-int is_myself(){
+uint8 is_myself(){
     return (field[head_x][head_y]==body_d) ||
             (field[head_x][head_y]==body_u) ||
             (field[head_x][head_y]==body_l) ||
@@ -130,43 +125,43 @@ void snake_head_update(){
             head_x = _mod8_bounded(head_x-1);
             head_y = head_y;
             if(is_myself()) is_dead = 1;
-            field[head_x][head_y] = _360_bounded(body_u-180);
+            field[head_x][head_y] = body_d;
             break;
         case right:
             head_x = head_x;
             head_y = _mod8_bounded(head_y+1);
             if(is_myself()) is_dead = 1;
-            field[head_x][head_y] = _360_bounded(body_r-180);
+            field[head_x][head_y] = body_l;
             break;
         case down:
             head_x = _mod8_bounded(head_x+1);
             head_y = head_y;
             if(is_myself()) is_dead = 1;
-            field[head_x][head_y] = _360_bounded(body_d-180);
+            field[head_x][head_y] = body_u;
             break;
         case left:
             head_x = head_x;
             head_y = _mod8_bounded(head_y-1);
             if(is_myself()) is_dead = 1;
-            field[head_x][head_y] = _360_bounded(body_l-180);
+            field[head_x][head_y] = body_r;
             break;
     }
 }
 
 
 void snake_clear(){
-    int i, j;
+    uint8 i, j;
     REP(i, 0, 7)
         REP(j, 0, 7)
             field[i][j] = 0;
 }
 
 void snake_body_update(){
+    uint8 x=head_x, y=head_y, x_temp, y_temp, i;
     if(is_dead) return;
-    int x=head_x, y=head_y, x_temp, y_temp, i;
     //Just update snake's body
     //snake_head_update() have to be called before!
-    if(_point_eq(apple_x, apple_y, head_x, head_y)){
+    if(_polong_eq(apple_x, apple_y, head_x, head_y)){
         // increase lenght of snake :v
         random_apple();
         return;
@@ -196,7 +191,7 @@ void snake_body_update(){
             //(x, y) is tail  --> remove
             field[x][y] = 0;
             //(x_temp, y_temp) --> new tail :v assign to ZERO
-            if(_point_eq(x_temp, y_temp, head_x, head_y))
+            if(_polong_eq(x_temp, y_temp, head_x, head_y))
                 field[x_temp][y_temp] = head;
             else
                 field[x_temp][y_temp] = tail;
@@ -211,7 +206,7 @@ void snake_initial(){
     random_XY(&head_x, &head_y);
     field[head_x][head_y]=head;
     random_apple();
-    current_direction = (random()%3)*90;
+    current_direction = (rand()%3)*90;
     next_direction = 0;
     is_dead = 0;
 }
